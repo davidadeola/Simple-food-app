ffac3cb49f8ab2c38257d7288b2aa7175bd67f5b
davidadeola_ <98563443+davidadeola@users.noreply.github.com>
2022-07-12 23:15:30 +0100

    First Commit

---
+++ src/components/Meals/MealItemForm.js
@@ -0,0 +1,23 @@
+import Input from '../UI/Input';
+import classes from './MealItemForm.module.css'
+
+const MealItemForm = (props) => {
+  const submitHandler = event => {
+    event.preventDefault();
+  }
+  return (
+    <form className={classes.form} onSubmit={submitHandler}>
+        <Input label='Amount' input={{
+            id: 'amount_' + props.id, //this changed!
+            type: 'number',
+            min: '1',
+            max: '5',
+            step: '1',
+            defaultValue: '1',
+        }} />
+        <button type='submit'>+ Add</button>
+    </form>
+  )
+}
+
+export default MealItemForm;
